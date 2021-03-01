e32657b9eb527570ffcad72de16471ddcab10dca
guilhermelopeseng <guilherme.lopes98@hotmail.com>
2021-03-01 15:50:44 -0300

    Update train and test

--- server.py
+++ server.py
@@ -2,4 +2,9 @@ from src.classification.train import TrainClass
 
 train = TrainClass()
 
-print(train.train())
+train.train() # comando para executar o treinamento, comentar caso não seja a necessidade 
+
+train.test() # comando para executar o test, comentar caso não seja a nesseidade 
+
+# para rodar basta executar no terminal o comando:
+# python server.py
--- src/classification/train.py
+++ src/classification/train.py
@@ -4,8 +4,14 @@ from pathlib import Path
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.applications import VGG19
+from tensorflow.keras import models, layers
+
+import matplotlib.pyplot as plt
+from mlxtend.plotting import plot_confusion_matrix
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 path_dataset = BASE_DIR/'ImageDataSet'
@@ -13,10 +19,10 @@ path_dataset = BASE_DIR/'ImageDataSet'
 class TrainClass():
   def __init__(self):
     self.batch_size   = 32
-    self.input_shape  = (150, 150, 3)
-    self.random_states = 42
+    self.input_shape  = (150, 150, 3) # esse valor tem que condizer com image.resize do arquivo datasetImage.py
+    self.random_states = 42 
     self.alpha        = 1e-5
-    self.epoch        = 60
+    self.epoch        = 10 # não colocar muitas épocas max 100
 
   # Essa função busca as imagens e os labels no banco de dados  
   def normalize(self): 
@@ -25,21 +31,23 @@ class TrainClass():
     labels = np.array(labels)
     lb = LabelBinarizer()
     labels = lb.fit_transform(labels) # transforma os labels em binário
-    return imagens, labels
+    
+    (trainX, testX, trainY, testY) = train_test_split(imagens, labels, test_size=0.20, stratify=labels, random_state=self.random_states) # separa os dados em treinamento e teste
 
-  # Essa função será usada para salvar os callbacks enquanto faz o treinamento
+    return trainX, testX, trainY, testY
+
+  # Essa função será usada para salvar os callbacks enquanto faz o treinamento, além de fazer o ajuste da taxa de apredizagem
   def checkpointFunction(self):
     filepath = 'tranferlearning_weights.hdf5'
     checkpoint = ModelCheckpoint(filepath,monitor='val_acc',verbose=1, save_best_only=True,mode='max')
-    lr_reduce = ReduceLROnPlateau(monitor='val_acc',factor=0.1, min_delta=self.alpha, patience=5, verbose=1)
+    lr_reduce = ReduceLROnPlateau(monitor='val_acc',factor=0.1, min_delta=self.alpha, patience=5, verbose=1)# ajuste lr
     callbacks = [checkpoint, lr_reduce]
     return callbacks
     
   # Função de treinamento
   def train(self):
-    imagens, labels = self.normalize()
-    
-    (trainX, testX, trainY, testY) = train_test_split(imagens, labels, test_size=0.20, stratify=labels, random_state=self.random_states) # separa os dados em treinamento e teste
+    trainX, testX, trainY, testY = self.normalize() # recebe os dados 
+    callbacks = self.checkpointFunction() #recebe os callbacks
 
     conv_base = VGG19(weights='imagenet',include_top=False, input_shape=self.input_shape) # pega o modelo da CNN VGG19
 
@@ -55,4 +63,60 @@ class TrainClass():
       else:
         layer.trainable = False
     
-    return conv_base.summary() 
+    # bloco referente as camadas do model
+    model = models.Sequential()
+    model.add(conv_base)
+    model.add(layers.GlobalAveragePooling2D())
+    model.add(layers.BatchNormalization())
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.6))
+    model.add(layers.Dense(3, activation='softmax'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+    # treinamento da CNN
+    history = model.fit(
+      trainX, 
+      trainY, 
+      batch_size=self.batch_size,
+      steps_per_epoch=len(trainX)//self.batch_size, 
+      validation_data=(testX, testY), 
+      validation_steps=len(testX)//self.batch_size,
+      callbacks=callbacks, 
+      epochs=self.epoch,
+      )
+
+    model.save('cnndiagnostic') # salvar os parâmetros da CNN
+
+    message_sucess = f'Treinamento realizado com sucesso!!' #Mensagem de sucesso na tela
+
+    return message_sucess 
+  
+  # Função de test da CNN
+  def test(self):
+    trainX, testX, trainY, testY = self.normalize() # busca os dados
+    filename = BASE_DIR/'cnndiagnostic' # caminho da cnn treinada
+    model = models.load_model(filename) # atribui o model da cnn treinada
+
+    pred = model.predict_classes(testX) # função para prever as classes a partir do vetor de testX
+    y_true = np.argmax(testY,axis = 1) # saída para fins de comparação da cnn
+
+    cm = confusion_matrix(y_true, pred) # Matriz de confusão da CNN
+    total = sum(sum(cm))
+    acc = (cm[0, 0] + cm[1, 1]+cm[2, 2]) / total
+    sensitivity_covid = cm[0, 0] / (cm[0, 0] + cm[0, 1] + cm[0, 2])
+    specificity_nocovid = cm[1, 1] / (cm[1, 0] + cm[1, 1] + cm[1, 2])
+    sensitivity_pneumonia = cm[2, 2] / (cm[2, 0] + cm[2, 1] + cm[2, 2])
+    # Posição 0: Covid-19
+    # Posição 1: No_findings
+    # Posição 2: Pneumonia 
+    
+    # mesagem de resposta na tela
+    print("Acurácia: {:.4f}".format(acc))
+    print("Sensitividade ao covid: {:.4f}".format(sensitivity_covid))
+    print("Sensitividade a pneumonia: {:.4f}".format(sensitivity_pneumonia))
+    print("Especificidade: {:.4f}".format(specificity_nocovid))
+
+    # gráfico da matriz de confusão
+    fig, ax = plot_confusion_matrix(conf_mat=cm ,  figsize=(5, 5))
+    plt.show()
+    
