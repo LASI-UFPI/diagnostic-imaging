2c31ef677554782d766568726a56a5a6f167b258
guilhermelopeseng <guilherme.lopes98@hotmail.com>
2021-03-31 10:54:14 -0300

    Update percent diagnostic and delete server

--- diagnosticApp/views.py
+++ diagnosticApp/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.contrib import messages
 from django.shortcuts import redirect
 
 from .forms import ImageModelForm
@@ -10,16 +9,33 @@ def index(request):
     form = ImageModelForm(request.POST, request.FILES)
     if form.is_valid():
       form.save()
-      message = f'Imagem enviada com sucesso, resultado dado pela Rede Neural convolucional é: \nCovid: {Image.objects.all().last().predict_covid:.2%}\nSem Detecção: {Image.objects.all().last().predict_no_findings:.2%}\nPneumonia: {Image.objects.all().last().predict_pneumonia:.2%}'
+
+      image = Image.objects.all().last().image
+      predict_covid = Image.objects.all().last().predict_covid*100
+      predict_no_findings = Image.objects.all().last().predict_no_findings*100
+      predict_pneumonia = Image.objects.all().last().predict_pneumonia*100
       # O resultado da CNN é dado pelo ultimo dado que entrou no banco de dados, isso pode ocorrer um bug, pensar em um jeito de passar o id do models no post_save para a view
-      messages.success(request=request, message=message)
-      form = ImageModelForm()
+
+      context = {
+        'form': form,
+        'image': image,
+        'form_valid': form.is_valid(),
+        'predict_covid': predict_covid,
+        'predict_no_findings': predict_no_findings,
+        'predict_pneumonia': predict_pneumonia,
+      }
+      return render(request, 'index.html', context)
     else:
-      messages.error(request, 'Erro ao enviar a imagem.')
+      error = 'Erro ao enviar'
+      context = {
+        'form': form,
+        'form_valid': form.is_valid(),
+      }
+      return render(request, 'index.html', context)
   else:
     form = ImageModelForm()
-  
-  context = {
-    'form': form
-  }
-  return render(request, 'index.html', context)
+    context = {
+      'form': form,
+      'form_valid': form.is_valid(),
+    }
+    return render(request, 'index.html', context)
